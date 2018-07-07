da5d985c2d44433d39797ee69c9ecb4994607382
Mike Redrobe <mike@redrobe.net>
2018-07-07 16:27:20 +0000

    Update dd-gui.py

--- dd-gui.py
+++ dd-gui.py
@@ -3,14 +3,15 @@
 
 from Tkinter import *
 import tkFileDialog
-from subprocess import *
-
+import subprocess
+import signal
 import thread
+import time
 
 app=Tk()
 
-source = "/dev/mmcblk0"
-dest = "~/backup.img"
+source = "/dev/sdd"
+dest = "backup.img"
 
 def choosesource():
     source = tkFileDialog.askopenfilename()
@@ -18,25 +19,18 @@ def choosedest():
     dest = tkFileDialog.askopenfilename()
 
 
-def StartDD():
-    ddproc = subprocess.Popen(['dd', 'if='+source, 'of='+dest], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def StartDD(self):
+    print source,dest
+    ddproc = subprocess.Popen(['sudo','dd', 'if='+source, 'of='+dest], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    while True:
+      line = ddproc.stderr.readline()
+      if line != '':
+        print line.rstrip()
+      else:
+        break
 
-    while ddproc.poll() is None:
-                        time.sleep(1)
-                        ddproc.send_signal(signal.SIGUSR1)
-                        while 1:
-                                l = ddproc.stderr.readline()
-                                if 'records in' in l:
-                                        progress = l[:l.index('+')]+' records '
-                                if 'bytes' in l:
-                                        temp = l.strip()
-                                        temp = temp[temp.index('(')+1:temp.index(')')]
-                                        status.insert(10, progress+" Copied: "+temp)
-                                        break
-
-
-def StartThread():
-    thread.start_new_thread(self.StartDD, (1,))
+def startThread():
+    thread.start_new_thread(StartDD,("",))
 
 
 label = Label(app,text="Read :").grid(row=0,column=0)
@@ -57,6 +51,6 @@ status.grid(row=2,column=1)
 entrysrc.insert(10,source)
 entrydest.insert(10,dest)
 
-button = Button(app,text="Start",command=StartThread).grid(row=3,column=2)
+button = Button(app,text="Start",command=startThread).grid(row=3,column=2)
 
 mainloop()
